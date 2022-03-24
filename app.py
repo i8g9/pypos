cd7690ff67a582d5b724485a60d6a37bf1e11c82
i8g9 <imazaryag@gmail.com>
2022-03-24 14:58:38 +0700

    [core] initializing static, templates and routing

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-from db import db_connection
+# from db import db_connection
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create')
+def create():
+    return render_template('createproduct.html')
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
+
+
